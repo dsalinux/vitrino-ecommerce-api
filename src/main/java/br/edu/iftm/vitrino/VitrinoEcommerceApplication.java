d3b7f4c5b749ee6e0d98569dfb942a5123c00dc7
Danilo Almeida <dsalinux@gmail.com>
2025-06-24 20:58:28 -0300

    Primeiro commit de criação do  Vitrino E-commerce API

---
+++ src/main/java/br/edu/iftm/vitrino/VitrinoEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.vitrino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrinoEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrinoEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1_1__CRIAR_USUARIO_ADMIN.sql
@@ -0,0 +1,2 @@
+INSERT INTO `vitrino_ecommerce`.`usuario` (`nome`, `email`, `senha`, `salt`) VALUES ('Admin', 'admin@iftm.edu.br', '123', '123');
+
