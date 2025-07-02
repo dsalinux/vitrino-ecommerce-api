0f31eb6e7e1490327d9dca5525afc57f50030607
Danilo Almeida <dsalinux@gmail.com>
2025-07-01 22:26:20 -0300

    Criação do crud usuario

---
+++ src/main/java/br/edu/iftm/vitrino/controller/UsuarioController.java
@@ -0,0 +1,71 @@
+package br.edu.iftm.vitrino.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.iftm.vitrino.entity.Usuario;
+import br.edu.iftm.vitrino.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	//CRUD
+	//Create
+	@PostMapping
+	public void criarUsuario(@RequestBody Usuario usuario) {
+		usuarioRepository.save(usuario);
+	}
+	
+	//Read
+	@GetMapping
+	public List<Usuario> listarUsuarios() {
+		return usuarioRepository.findAll();
+	}
+	//Read
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+		Usuario usuario = usuarioRepository.findById(id).orElse(null);
+		if (usuario == null) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(usuario);
+		}
+	}
+	//Read
+	@GetMapping("/email/{email}")
+	public Usuario buscarUsuarioPorEmail(@PathVariable String email) {
+		return usuarioRepository.findByEmail(email);
+	}
+	
+	//Update
+	@PutMapping("/{id}")
+	public void atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+		if (usuarioRepository.existsById(id)) {
+			usuario.setId(id);
+			usuarioRepository.save(usuario);
+		}
+	}
+	
+	//Delete
+	@DeleteMapping("/{id}")
+	public void deletarUsuario(@PathVariable Long id) {
+		if (usuarioRepository.existsById(id)) {
+			usuarioRepository.deleteById(id);
+		}
+	}
+	
+}
