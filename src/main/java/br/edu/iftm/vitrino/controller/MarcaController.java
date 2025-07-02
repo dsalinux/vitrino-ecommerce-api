e8faa6701aa3cd3917292326f4d26addeac6a691
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-02 20:24:02 -0300

    Removi o @Autowired, adicionei o PutMapping e o DeleteMapping

--- src/main/java/br/edu/iftm/vitrino/controller/MarcaController.java
+++ src/main/java/br/edu/iftm/vitrino/controller/MarcaController.java
@@ -2,10 +2,10 @@ package br.edu.iftm.vitrino.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +19,6 @@ import br.edu.iftm.vitrino.repository.MarcaRepository;
 @RequestMapping("/marca")
 public class MarcaController {
 
-    @Autowired
     private final MarcaRepository marcaRepository;
 
     public MarcaController(MarcaRepository marcaRepository) {
@@ -36,13 +35,13 @@ public class MarcaController {
         return ResponseEntity.ok(marcaRepository.findAll());
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/alterar/{id}")
     public ResponseEntity<Marca> atualizarMarca(@RequestBody Marca novaMarca) {
         return ResponseEntity.ok(marcaRepository.save(novaMarca));
     }
 
-    @DeleteMapping("{id}")
-    public void excluirMarca(Long id) {
+    @DeleteMapping("/deletar/{id}")
+    public void excluirMarca(@PathVariable Long id) {
         marcaRepository.deleteById(id);
     }
 }
