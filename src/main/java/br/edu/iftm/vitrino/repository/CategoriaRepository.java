a1db9a3d901e4c19ce278f314b9fdad94cf20dbd
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-02 20:26:57 -0300

    Só formatei o código e alterei para Long o id de categoria.

--- src/main/java/br/edu/iftm/vitrino/repository/CategoriaRepository.java
+++ src/main/java/br/edu/iftm/vitrino/repository/CategoriaRepository.java
@@ -1,10 +1,8 @@
 package br.edu.iftm.vitrino.repository;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.iftm.vitrino.entity.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
