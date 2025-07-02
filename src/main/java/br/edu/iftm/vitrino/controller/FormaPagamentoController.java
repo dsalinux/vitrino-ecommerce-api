a36e56b351bfa44f19874fbf2ebab53cf3439499
BrenoMatayoshi <brenomasato@gmail.com>
2025-07-02 16:25:00 -0300

    crud forma de pagamento concluido

--- src/main/java/br/edu/iftm/vitrino/controller/FormaPagamentoController.java
+++ src/main/java/br/edu/iftm/vitrino/controller/FormaPagamentoController.java
@@ -3,20 +3,57 @@ package br.edu.iftm.vitrino.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.vitrino.entity.FormaPagamento;
 import br.edu.iftm.vitrino.repository.FormaPagamentoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@RestController
+@RequestMapping("/forma_pagamento")
 public class FormaPagamentoController {
-    @Autowired
+	@Autowired
 	private FormaPagamentoRepository formaPagamentoRepository;
-	
-	//@GetMapping("/forma_pagamento")
-	//public List<FormaPagamento> listarFluxoCaixa() {
-	//	return formaPagamentoRepository.findAll();
-	//}
 
-    //Pontuar os outros possivel controllers para as Formas de Pagamento
+	@GetMapping
+	public List<FormaPagamento> listarFluxoCaixa() {
+		return formaPagamentoRepository.findAll();
+	}
+
+	@PostMapping
+	public void criarFormaPagamento(@RequestBody FormaPagamento formaPagamento) {
+		formaPagamentoRepository.save(formaPagamento);
+	}
+
+	@PutMapping("/{id}")
+	public void postMethodName(@RequestBody Long id, @RequestBody FormaPagamento formaPagamento) {
+		if (formaPagamentoRepository.existsById(id)) {
+			formaPagamento.setId(id);
+			formaPagamentoRepository.save(formaPagamento);
+		}
+	}
+	
+	public ResponseEntity<FormaPagamento> buscarFaluxoPorId(@RequestParam Long id) {
+		FormaPagamento formaPagamento = formaPagamentoRepository.findById(id).orElse(null);
+		if (formaPagamento != null) {
+			return ResponseEntity.ok(formaPagamento);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public void deletarUsuario(@PathVariable Long id) {
+		if (formaPagamentoRepository.existsById(id)) {
+			formaPagamentoRepository.deleteById(id);
+		}
+	}
 }
