278e8835a010d40fb223f554f54836eff6575f30
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-01 21:56:01 -0300

    adicionei opções extras no ProdutoRepository para procurar pelo nome, procurar pelo id, deletar por id, deletar por id.

--- src/main/java/br/edu/iftm/vitrino/repository/ProdutoRepository.java
+++ src/main/java/br/edu/iftm/vitrino/repository/ProdutoRepository.java
@@ -1,9 +1,20 @@
 package br.edu.iftm.vitrino.repository;
 
+import java.util.Optional; // Garanta que Optional está importado
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository; // Adicione esta importação para boas práticas
 
 import br.edu.iftm.vitrino.entity.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Optional<Produto> findByNome(String nome);
+
+    Optional<Produto> findById(Long id);
+
+    void deleteById(Long id);
+
+    void delete(Long id);
 }
