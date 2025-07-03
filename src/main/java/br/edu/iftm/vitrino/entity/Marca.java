f1de2d6d98f00ee128eeb98dd1dd946cad17a2c8
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-03 10:23:08 -0300

    adicinando GeneratedValue na classe marca.

--- src/main/java/br/edu/iftm/vitrino/entity/Marca.java
+++ src/main/java/br/edu/iftm/vitrino/entity/Marca.java
@@ -2,6 +2,8 @@ package br.edu.iftm.vitrino.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,6 +20,7 @@ import lombok.Setter;
 public class Marca {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "marca")
