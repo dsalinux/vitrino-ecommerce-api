7e68b71740e1ba4d5a14d150d39fac09006ed6b5
Luiz Otavio <luizzotavionazar@outlook.com>
2025-07-02 23:35:42 -0300

    rmeoção de comentarios

--- src/main/java/br/edu/iftm/vitrino/entity/FormaPagamento.java
+++ src/main/java/br/edu/iftm/vitrino/entity/FormaPagamento.java
@@ -1,10 +1,7 @@
 package br.edu.iftm.vitrino.entity;
 
 import jakarta.persistence.Entity;
-
-
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,7 +12,6 @@ import lombok.Data;
 @Table(name = "forma_pagamento")
 @Data
 public class FormaPagamento {
-    // Nome e id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
