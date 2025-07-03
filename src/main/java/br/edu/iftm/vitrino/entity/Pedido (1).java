980112d26b6845e215d4f26b3f8aedfdbd8506c6
Helem <helembritoanjos@gmail.com>
2025-07-02 22:29:02 -0300

    Concluídas as entidades Pedido e ItemPedido

---
+++ src/main/java/br/edu/iftm/vitrino/entity/Pedido (1).java
@@ -0,0 +1,14 @@
+package br.edu.iftm.vitrino.entity;
+
+import java.time.LocalDateTime;
+
+public class Pedido {
+	
+	private Long id;
+	private Long cliente_id;
+	private LocalDateTime data_registro;
+	private Long endereco_entrega_id;
+	private LocalDateTime data_cancelamento;
+	
+
+}
--- src/main/java/br/edu/iftm/vitrino/entity/Pedido.java
+++ src/main/java/br/edu/iftm/vitrino/entity/Pedido.java
@@ -1,13 +1,15 @@
 package br.edu.iftm.vitrino.entity;
 
-import java.security.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,15 +19,22 @@ import lombok.Data;
 public class Pedido {
 	
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;
-	private Long cliente_id;
-	@Column(name="data_registro")
-	private Timestamp dataRegistro;
-	private Long endereco_entrega_id;
-	@Column(name="data_cancelamento")
-	private LocalDateTime dataCancelamento;
-	
-	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(name = "cliente_id")
+    private Integer clienteId;
+
+    @Column(name = "data_registro")
+    private LocalDateTime dataRegistro;
+
+    @Column(name = "endereco_entrega_id")
+    private Integer enderecoEntregaId;
+
+    @Column(name = "data_cancelamento")
+    private LocalDateTime dataCancelamento;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens;
+	
 }
