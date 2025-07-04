4fd646cb442a8eab579636834c03367976b3acb8
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-04 07:50:22 -0300

    UsuarioController estava fora da pasta controller

---
+++ src/main/java/br/edu/iftm/vitrino/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package br.edu.iftm.vitrino.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.iftm.vitrino.entity.Usuario;
+import br.edu.iftm.vitrino.repository.UsuarioRepository;
+
+@RestController
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping("/usuarios")
+	public List<Usuario> listarUsuarios() {
+		return usuarioRepository.findAll();
+	}
+
+}
