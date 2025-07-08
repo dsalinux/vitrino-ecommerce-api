f40caeabed1ac226e1384c977e813a68dda35517
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-08 08:28:15 -0300

    trocando a coluna do banco de dados de String nome, para String marca

--- src/main/java/br/edu/iftm/vitrino/entity/Marca.java
+++ src/main/java/br/edu/iftm/vitrino/entity/Marca.java
@@ -23,6 +23,7 @@ public class Marca {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome")
+    @Column(name = "marca")
     private String nome;
+
 }
