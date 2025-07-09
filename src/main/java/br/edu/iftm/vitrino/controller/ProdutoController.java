077b4c5efcc3e051fdedc09f0c2d71e66e315493
Marcelo Paixão <marceloaugusto10123@gmail.com>
2025-07-09 10:18:13 -0300

    feat: add anotation to handle with cors

--- src/main/java/br/edu/iftm/vitrino/controller/ProdutoController.java
+++ src/main/java/br/edu/iftm/vitrino/controller/ProdutoController.java
@@ -4,19 +4,13 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus; // Para usar HttpStatus.CREATED
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.edu.iftm.vitrino.entity.Produto;
 import br.edu.iftm.vitrino.repository.ProdutoRepository; // Continue usando o repositório
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/produto")
 public class ProdutoController {
 
--- src/main/java/br/edu/iftm/vitrino/controller/UsuarioController.java
+++ src/main/java/br/edu/iftm/vitrino/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.edu.iftm.vitrino.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import br.edu.iftm.vitrino.entity.Usuario;
 import br.edu.iftm.vitrino.repository.UsuarioRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
 
 	@Autowired
